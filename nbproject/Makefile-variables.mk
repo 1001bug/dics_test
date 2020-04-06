b05cfd19048796550a04b0ec43a78baccf58a047
1001bug <41540294+1001bug@users.noreply.github.com>
2020-04-07 01:53:46 +0300

    netbeans project

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=dics_test
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/dics_test
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=dicstest.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/dicstest.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=dics_test
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/dics_test
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=dicstest.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/dicstest.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
